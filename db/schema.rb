6e2aedd782eee73e0a72809b54f5cf3f1c44d11b
Calven Wong <calven.wong@grabtaxi.com>
2022-06-30 15:05:31 +0800

    added some styling for notes page and testing its routes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_29_160333) do
+ActiveRecord::Schema.define(version: 2022_06_30_061950) do
 
   create_table "notebooks", force: :cascade do |t|
     t.string "title"
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 2022_06_29_160333) do
 
   create_table "pages", force: :cascade do |t|
     t.string "title"
-    t.string "position"
+    t.integer "position"
     t.integer "notebook_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
