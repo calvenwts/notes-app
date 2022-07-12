1f5fc33fdda1576d2210ac2b6c6282d90a569d82
Calven Wong <calven.wong@grabtaxi.com>
2022-07-12 13:36:38 +0800

    added click and render for pages module

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,3 +16,22 @@ ActiveStorage.start()
 
 require("trix")
 require("@rails/actiontext")
+
+document.addEventListener('turbolinks:load', () => {
+    document.addEventListener('click', (event)=> {
+        let element = event.target.closest('.text-content')
+        if (!element) return;
+
+        element.classList.add('d-none');
+        element.nextElementSibling.classList.remove('d-none')
+    })
+
+    document.addEventListener('click', (event)=> {
+        if (!event.target.matches('.cancel')) return;
+
+        let element = event.target.closest('.form-content')
+
+        element.classList.add('d-none');
+        element.previousElementSibling.classList.remove('d-none')
+    })
+})
